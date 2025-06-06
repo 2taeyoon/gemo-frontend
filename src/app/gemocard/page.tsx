bd71367b921d0bbe020d4bf847744f3933d8e5a5
2taeyoon <121344457+2taeyoon@users.noreply.github.com>
2025-06-06 21:32:46 +0900

    Add Phaser card game

--- src/app/gemocard/page.tsx
+++ src/app/gemocard/page.tsx
@@ -1,24 +1,17 @@
-import React from 'react'
-import Gemocard from "@/app/gemocard/gemocard";
+import React from 'react';
+import Gemocard from '@/app/gemocard/Gemocard';
 
 export const metadata = {
-  title: "GEMO",
-  description: "시간 때울 때 사용하기 좋은 게임 GEMO입니다.",
-	keywords: ["GEMO", "게임", "시간 때우기"],
+  title: 'GEMO',
+  description: '시간 때울 때 사용하기 좋은 게임 GEMO입니다.',
+  keywords: ['GEMO', '게임', '시간 때우기'],
   openGraph: {
-    title: "GEMO",
-    description: "시간 때울 때 사용하기 좋은 게임 GEMO입니다.",
-    // url: "https://www.2taeyoon.com/",
-    // images: [
-    //   {
-    //     url: "https://www.2taeyoon.com/favicon/main_meta_image.png",
-    //     alt: "Profile Thumbnail",
-    //   },
-    // ],
-    type: "website",
+    title: 'GEMO',
+    description: '시간 때울 때 사용하기 좋은 게임 GEMO입니다.',
+    type: 'website',
   },
 };
 
-export default function page() {
-	return <Gemocard/>
+export default function Page() {
+  return <Gemocard />;
 }
