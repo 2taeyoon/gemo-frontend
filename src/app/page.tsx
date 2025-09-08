72168672ea01dd9749520a21a6b8a54cb71c1c70
2taeyoon <2tae@kakao.com>
2025-09-09 01:02:22 +0900

    feat: 메인 페이지 사용자 정보 및 레벨바 표시 개선 - 로그인 상태에 따른 사용자 환영 메시지 추가 및 레벨바 최대 레벨 도달 시 메시지 표시, 레이아웃 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,47 +19,59 @@ export default function page() {
 
 
   return (
-    <div className="main_section">
-      {/* <Link href="/auth" className="login_button_wrap">
-        <div className="login_logo"></div>
-        <p className="h3text">로그인 하러가기</p>
-      </Link> */}
+    <>
+			<div className="main_section">
+				{/* <Link href="/auth" className="login_button_wrap">
+					<div className="login_logo"></div>
+					<p className="h3text">로그인 하러가기</p>
+				</Link> */}
 
 
-      {/* 로딩 상태 표시 */}
-      {status === "loading" && ( <div style={{ color: 'var(--foreground)' }}>로딩 중...</div> )}
+				{/* 로딩 상태 표시 */}
+				{status === "loading" && ( <div style={{ color: 'var(--foreground)' }}>로딩 중...</div> )}
 
-      {/* 로그인된 사용자 정보 및 레벨바 */}
-      {status === "authenticated" && session?.user && (
-        <div className="login_button_wrap authenticated">
+				{/* 로그인한 사용자 정보 및 레벨바 START! */}
+				{status === "authenticated" && session?.user && (
+					<div className="login_button_wrap authenticated">
 
-					{/* 레벨바 로딩 중 */}
-          {loading && (
-						<div style={{color: 'var(--foreground)'}}>레벨 로딩 중...</div>
-					)}
+						<div className="login_button_wrap_content">
+								{/* 레벨바 로딩 중 */}
+								{loading && (
+									<div style={{color: 'var(--foreground)'}}>레벨 로딩 중...</div>
+								)}
 
-          {/* 사용자 정보 표시 */}
-          <div style={{color: 'var(--foreground)'}}>{session.user.email || session.user.name}</div>
+								{/* 사용자 이메일 OR 닉네임 표시 */}
+								<div style={{color: 'var(--foreground)'}}>{session.user.email || session.user.name}님 환영합니다!</div>
 
-					{/* 레벨바 표시 */}
-					{ user && !loading && ( 
-							<div>
-								<LevelBar showXpText={true} levleInfo={false} tooltip={false}/>
-							</div>
-						)}
+								{/* 레벨바 로그아웃 버튼 표시 */}
+								{ user && !loading && ( 
+									<div>
+										<LevelBar showXpText={true} levleInfo={false} tooltip={false}/>
+										<button onClick={handleLogout} title="로그아웃" style={{color: 'var(--foreground)'}}>로그아웃</button>
+									</div>
+								)}
+						</div>
 
-					{/* 로그아웃 버튼 */}
-          <button onClick={handleLogout} title="로그아웃" style={{color: 'var(--foreground)'}}>로그아웃</button>
-        </div>
-      )}
+					</div>
+				)}
+				{/* 로그인한 사용자 정보 및 레벨바 END! */}
 
-      {/* 로그인되지 않은 경우 로그인 버튼 표시 */}
-      {status === "unauthenticated" && (
-        <Link href="/auth" className="login_button_wrap not_authenticated">
-          <div className="login_logo"></div>
-          <p className="h3text">로그인 하러가기</p>
-        </Link>
-      )}
-    </div>
+				{/* 로그인되지 않은 경우 로그인 버튼 표시 START! */}
+				{status === "unauthenticated" && (
+					<Link href="/auth" className="login_button_wrap not_authenticated">
+						<div className="login_logo"></div>
+						<p className="h3text">로그인 하러가기</p>
+					</Link>
+				)}
+				{/* 로그인되지 않은 경우 로그인 버튼 표시 END! */}
+			</div>
+
+
+
+			{/* 준비중입니다. 표시 */}
+			<div style={{padding: '4.0rem' }}>
+				<div style={{background: '#eee', height: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.0rem', fontWeight: '700' }}>준비중입니다.</div>
+			</div>
+		</>
   );
 }
